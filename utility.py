5a5ebb537de6c8d72ea6d88d742934788cfae136
CornerSiow <77437729+CornerSiow@users.noreply.github.com>
2023-07-27 10:49:33 +0900

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import *
 def showScore(y_true, y_pred):
-  print("mean absolute error:",{:.5f}.format(mean_absolute_error(y_true, y_pred)))
-  print("r2 score:",{:.5f}.format(r2_score(y_true, y_pred)))
-  print("explained variance score",{:.5f}.format(explained_variance_score(y_true, y_pred)))
+  print("mean absolute error:","{:.5f}".format(mean_absolute_error(y_true, y_pred)))
+  print("r2 score:","{:.5f}".format(r2_score(y_true, y_pred)))
+  print("explained variance score","{:.5f}".format(explained_variance_score(y_true, y_pred)))
